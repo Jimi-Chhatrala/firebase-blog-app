8ef145dcdaad70e328ddf6fcaf8bdaaecde995d2
jimi-chhatrala <jimichhatrala8488@gmail.com>
2024-07-27 22:23:14 +0530

    Blog Home Section Completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,48 @@
-import React from "react";
+import { collection, onSnapshot } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
+import BlogSection from "../components/BlogSection";
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const unsub = onSnapshot(
+      collection(db, "blogs"),
+      (snapshot) => {
+        let list = [];
+        snapshot.docs.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+        });
+        setBlogs(list);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    return () => {
+      unsub();
+    };
+  }, []);
+
+  console.log("blogs", blogs);
+
   return (
-    <div>
-      <h2 className="text-primary">Home</h2>
+    <div className="container-fluid pb-4 pt-4 padding">
+      <div className="container padding">
+        <div className="row mx-0">
+          <h2>Trending</h2>
+          <div className="col-md-8">
+            <BlogSection blogs={blogs} />
+          </div>
+          <div className="col-md-3">
+            <h2>Tags</h2>
+            <h2>Most Popular</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
